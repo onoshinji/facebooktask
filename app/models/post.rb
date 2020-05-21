3591a7a44efc5c16aee340f6fbec989d12f87935
ono shinji <yjsnpi555@gmail.com>
2020-05-21 18:08:19 +0900

    投稿機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 255 }
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy,:show]
   resources :users, only: [:new, :create, :destroy,:show]
-  # resources :posts
+  resources :posts
 end
