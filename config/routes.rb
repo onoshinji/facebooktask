13c8ad26ffd1fef050ddf17acb93576983d60e0c
ono shinji <yjsnpi555@gmail.com>
2020-05-21 15:32:09 +0900

    ログインフォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :destroy]
   resources :posts
 end
