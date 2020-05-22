896569e07a555125e37cbc978a95b71170e97b1f
ono shinji <yjsnpi555@gmail.com>
2020-05-22 14:01:14 +0900

    画像投稿機能を実装したが、偉p」

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 255 }
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
